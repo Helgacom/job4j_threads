60025000fe8d42453168dbd0fe451afd4d197fad
Olga Remneva <remneva_olga@mail.ru>
2024-01-17 13:12:56 +0400

    ThreadLocal

---
+++ src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,17 @@
+package ru.job4j.threads.threadlocal;
+
+public class ThreadLocalDemo {
+    /* as a rule - private static*/
+    public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new FirstThread();
+        Thread second = new SecondThread();
+        threadLocal.set("Это поток main.");
+        System.out.println(threadLocal.get());
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+}
